0baeb9bc102330c56264e19ba784b904e21fdbba
DylanHill28 <43118239+DylanHill28@users.noreply.github.com>
2019-02-19 22:14:29 -0600

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-19
+OU ID_______________: 113419933
+OU EMAIL____________: cdhill@ou.edu
+
+Tasks:
+1. Complete Calulator.java
+2. Complete CalculatorTest.java
+
+Estimated Time (minutes):
+1. 60
+2. 45
+
+Actual Time (minutes):
+1. 60
+2. 60
+*/}
